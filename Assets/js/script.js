075564178b1e973685998053617579f5587f2c2f
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-26 14:06:32 -0500

    finished card generating base only

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,17 +3,85 @@
 // then the current weather info and 5 day forecast is revealed
 
 
-// fetch geocode data from google maps api, it will convert input city to lat and lon
-// assign the variables to lat and long, es6 wll pass in the lat and long to the next api openweather map call, get the results and display it
-// time variable will get the current time through date object
+// fetch current day data from current weather data api
+
+// convert kelvin to fahrenheight
+
+//(K − 273.15) × 9/5 + 32 
+
 
 var lat = "";
 var lon = "";
-const moment = require(moment);
 
-var apiURLOne = fetch(`https: //api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=3f3c84db569188d2c7dd0abf10be0fd4`).then(
+var cityName = "";
+
+var getData = function(cityName) {
+    var card = [];
+
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3f3c84db569188d2c7dd0abf10be0fd4`).then(response => response.json()).then(data => {
+        console.log(data);
+        console.log(data.main.temp);
+        temperatureInFahrenheight = (data.main.temp - 273.15) * 9 / 5 + 32
+        document.getElementById('temp').textContent = temperatureInFahrenheight;
+        document.getElementById('wind').textContent = data.wind.speed;
+        document.getElementById('humidity').textContent = data.main.humidity;
+        console.log(data.coord.lon);
+        lon = data.coord.lon;
+        lat = data.coord.lat;
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=3f3c84db569188d2c7dd0abf10be0fd4`).then(response => response.json()).then(data2 => {
+                console.log(data2)
+                document.getElementById('uv').textContent = data2.current.uvi;
+                var daily = document.getElementById('fiveDays');
+
+                for (i = 0; i < 5; i++) {
+                    card[i] = document.createElement('div');
+                    card[i].setAttribute('class', 'card');
+                    iToString = i.toString();
+                    console.log(typeof(iToString))
+                    card[i].setAttribute('id', 'card' + iToString);
+                    daily.appendChild(card[i]);
+                    var cardById = document.getElementById('card' + iToString);
+                    h3 = document.createElement('h3');
+                    cardById.appendChild(h3);
+                    if (card[i] === 0) {
+                        var unixTime = data2.current.dt;
+                        var date = new Date(unixTime * 1000);
+                        h3.textContent = date.toLocaleDateString("en-US");
+                    } else {
+                        var unixTime = data2.daily[i].dt;
+                        var date = new Date(unixTime * 1000);
+                        h3.textContent = date.toLocaleDateString("en-US");
+
+                    }
+                }
 
 
 
 
-)
+            }
+
+        ).catch(err => console.error(err));
+
+    }).catch(err => console.error(err));
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+document.getElementById('search').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var cityName = document.getElementById('searchInput').value;
+    getData(cityName);
+
+})
